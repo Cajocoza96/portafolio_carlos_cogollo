6ecc1ceab18957b29c79a4225535600de9722d1d
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-08-12 15:26:18 -1200

    puse un h-[100svh]

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import useIsMobile from "./hooks/useIsMobile";
 import windowsEscritorioHorizontal from "/assets/img/escritorio/normal/windowsEscritorioHorizontal.webp";
 import windowsEscritorioVertical from "/assets/img/escritorio/normal/windowsEscritorioVertical.webp";
 
-import OptimizedImage, { IMAGE_CONFIGS }  from "./componentes/common/OptimizedImage";
+import OptimizedImage, { IMAGE_CONFIGS } from "./componentes/common/OptimizedImage";
 import BarraDeTareas from "./componentes/Barra_de_tareas/BarraDeTareas";
 
 export default function App() {
@@ -15,23 +15,27 @@ export default function App() {
   const imgAlt = isMobile ? "Welcome vertical view" : "Welcome horizontal view";
 
   return (
-    <div className="h-[100svh] flex flex-col justify-end">
-        
-      <div className="w-full">
-        <OptimizedImage
-          src={imgSrc}
-          alt={imgAlt}
-          className="w-screen brightness-60 dark:brightness-50"
-          asBackground={true}
-          backgroundSize="cover"
-          backgroundPosition="center"
-          minHeight="100svh"
-          showSkeleton={false}
-          {...IMAGE_CONFIGS.CRITICAL}
-        />
-      </div>
+    <div className="h-[100svh]">
 
-      <BarraDeTareas />
+      <div className="h-[100svh] flex flex-col justify-end">
+
+        <div className="w-full">
+          <OptimizedImage
+            src={imgSrc}
+            alt={imgAlt}
+            className="w-screen brightness-60 dark:brightness-50"
+            asBackground={true}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            minHeight="100svh"
+            showSkeleton={false}
+            {...IMAGE_CONFIGS.CRITICAL}
+          />
+        </div>
+
+        <BarraDeTareas />
+
+      </div>
 
     </div>
 
