79bb58beb7c977ebf27cab1a97e4837684f5a63d
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-09-02 16:16:22 -1200

    3 correcion zoom

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import Escritorio from "./componentes/Escritorio/Escritorio";
 
 export default function App() {
   return (
-    <div className="bg-blue-950 dark:bg-gray-950 h-[100svh] flex flex-col justify-end">
+    <div className="bg-blue-950 dark:bg-gray-950 min-h-screen h-screen max-h-screen w-full overflow-hidden flex flex-col justify-end fixed inset-0">
       <Escritorio />
     </div>
-
   );
 }
